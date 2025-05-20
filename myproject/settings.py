3e2d77dd680e73c405a39f87e1866924adbdc0d2
Данил Щербаков <danilshcherbakov@MacBook-Air-Danil.local>
2025-05-20 19:41:10 +0500

    Настройка PostgreSQL с переменными окружения и SSL-сертификатом

--- myproject/settings.py
+++ myproject/settings.py
@@ -63,8 +63,16 @@ WSGI_APPLICATION = 'myproject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': os.environ.get('DB_ENGINE', 'django.db.backends.postgresql_psycopg2'),
+        'HOST': os.environ.get('DB_HOST', ''),
+        'NAME': os.environ.get('DB_NAME', ''),
+        'USER': os.environ.get('DB_USER', ''),
+        'PASSWORD': os.environ.get('DB_PASSWORD', ''),
+        'PORT': os.environ.get('DB_PORT', '6432'),
+        'OPTIONS': {
+            'sslmode': 'verify-full',
+            'sslrootcert': 'root.crt',
+        },
     }
 }
 
